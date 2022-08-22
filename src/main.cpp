622cc84e24dc2d7d2f2e353ee67b760732bd0ac9
Đorđe Jocić <office@djordjejocic.com>
2022-08-22 11:17:11 +0200

    Better API.

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,24 @@ int main(int argc, char *argv[]) {
     int set[] = { 1, 2, 3, 4 };
     int set_len = sizeof(set) / sizeof(int);
 
-    int *combo = (int*)calloc(set_len, sizeof(int));
-    int  combo_len = set_len;
+    int *combo;
+    int  combo_len;
 
-    Combinations c(set, set_len, 1, 3);
+    Combinations c(&set[0], set_len, "1-3");
+    //Combinations c(&set[0], set_len, 1, 3);
+    //Combinations c(&set[0], set_len, 2);
+
+    combo = c->init(&combo_len);
 
     while (c.has_next()) {
-        c.next(combo, combo_len, Combinations::BY_VALUE); // OR Combinations::BY_INDICES
-        c.print(combo, combo_len);
+
+        c.next(Combinations::BY_VALUE);
+        //c.next(Combinations::BY_INDICES);
+
+        c.print();
+
+        for (int i = 0; i < combo_len; i++) {
+            // do stuff
+        }
     }
 }
